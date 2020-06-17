a9978c456256caffdf8ad6b7ddfee0827a268bd4
1984204091@qq.com <1984204091@qq.com>
2020-06-17 20:19:47 +0800

    路由认识，模板引擎下载

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      redirect: "/login",
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () =>
+        import("./views/login/index.vue")
+    }
+  ]
+});
